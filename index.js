22c684c4ac2b042697e818455b10adf545cd1e05
Manoj Kumar <70743261+manojkumar8400@users.noreply.github.com>
2021-12-10 10:18:35 +0530

    add 1 more question

--- index.js
+++ index.js
@@ -23,26 +23,24 @@ function play(question,answer){
 
 // Data of Highscores
 
-var highScore=[{
-  'Bipin':3,
-},
-{
-  'Nikhil':3
-}];
+var highScore={
+  'Bipin':5,
+}
 
 // Call Function
 
 play("Where i am from?  \n","jhansi")
 
-play("My favourite Avenger's superhero?  \n","Ironman")
+play("My favourite Avenger's superhero ?  \n","Ironman")
 
-play("Where i am doing study \n","dharamshala")
+play("Where i am doing study ? \n","dharamshala")
 
-play("What is he doing in Dharamshala?  \n","Study")
+play("What is he doing in Dharamshala ?  \n","Study")
 
+play("which is my bollywood favorite movie ? \n","3 idiots")
 
 // For Print Output
 
 console.log(userName,"Your final Score is ",score)
-console.log("High Scores -:",highScore[0],highScore[1])
+console.log("High Score -:",highScore["Bipin"])
 
